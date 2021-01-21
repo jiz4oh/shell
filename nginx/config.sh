02399ba9d04556c7c45a0d615d43482898deeba2
jiz4oh <jiz4oh@gmail.com>
2021-01-21 16:22:13 +0800

    fix: config nginx resources not found

--- nginx/config.sh
+++ nginx/config.sh
@@ -1,5 +1,5 @@
 mkdir -p /etc/nginx/{conf,conf.d,sites-enabled}
-repo_url=https://raw.githubusercontent.com/jiz4oh/shell/nginx/resources
+repo_url=https://raw.githubusercontent.com/jiz4oh/shell/master/nginx/resources
 
 nginx_conf=$repo_url/nginx.conf
 mime_types=$repo_url/mime.types
