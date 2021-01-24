1dae8207b072e9b80304e3036c21aabc15aa2366
jiz4oh <jiz4oh@gmail.com>
2021-01-24 12:07:21 +0800

    write the correct code

--- ffmpeg/multi_trans.sh
+++ ffmpeg/multi_trans.sh
@@ -3,7 +3,7 @@
 
 input_format=$1
 output_format=$2
-files=`ls | grep $input_format | awk -F. '{for(i=1;i<=NF-1;i++){if(i==1){res=$1}else{res=res"."$i;}}print res;}'| awk '{for(i=1;i<=NF;i++){if(i==1){res=$1}else{res=res"\ "$i;}}print res;}'`
+files=$(find ./*"$input_format"* | awk -F. '{for(i=1;i<=NF-1;i++){if(i==1){res=$1}else{res=res"."$i;}}print res;}'| awk '{for(i=1;i<=NF;i++){if(i==1){res=$1}else{res=res"\ "$i;}}print res;}')
 
 old_IFS=$IFS
 IFS=$'\t\n'
@@ -11,6 +11,8 @@ IFS=$'\t\n'
 for i in $files
 do
  input_file=$i.$input_format
- output_file=`echo $i | awk '{for(i=1;i<=NF;i++){res=res$i;}print res;}'`.$output_format
- ffmpeg -i $input_file -y $output_file
+ output_file=$(echo "$i" | awk '{for(i=1;i<=NF;i++){res=res$i;}print res;}').$output_format
+ ffmpeg -i "$input_file" -y "$output_file"
 done
+
+IFS=$old_IFS
