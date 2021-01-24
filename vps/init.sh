636e2e6c1ab429249ed8f84a572ff1603aae8922
jiz4oh <jiz4oh@gmail.com>
2021-01-24 12:01:23 +0800

    使用 utils/log.sh 改写 vps/init.sh

--- vps/init.sh
+++ vps/init.sh
@@ -1,5 +1,6 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
+curl -fsSL 'https://raw.githubusercontent.com/jiz4oh/shell/master/utils/log.sh' -o .tmp.sh && source .tmp.sh && rm .tmp.sh
 
 #=================================================
 #	System Required: CentOS/Debian/Ubuntu
@@ -17,34 +18,6 @@ export PATH
 
 timezone=Asia/Shanghai
 new_ssh_port=23232
-
-Red_font_prefix="\033[31m"
-Green_font_prefix="\033[32m"
-Yellow_font_prefix="\033[33m"
-Blue_font_prefix="\033[34m"
-Azure_font_prefix="\033[36m"
-
-Font_color_suffix="\033[0m"
-
-function LOG_DEBUG() {
-  echo -e "${Azure_font_prefix}[Debug] $1${Font_color_suffix}"
-}
-
-function LOG_INFO() {
-  echo -e "${Green_font_prefix}[Info] $1${Font_color_suffix}"
-}
-
-function LOG_NOTICE() {
-  echo -e "${Blue_font_prefix}[Notice] $1${Font_color_suffix}"
-}
-
-function LOG_WARNING() {
-  echo -e "${Yellow_font_prefix}[Warning] $1${Font_color_suffix}"
-}
-
-function LOG_ERROR() {
-  echo -e "${Red_font_prefix}[Error] $1${Font_color_suffix}"
-}
 
 function setTimezone() {
   LOG_INFO "set $1 as timezone"
