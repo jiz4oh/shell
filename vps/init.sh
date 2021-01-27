8a1a48fa490fd2c86fe6507022979f691107bf97
jiz4oh <jiz4oh@gmail.com>
2021-01-27 10:13:07 +0800

    避免不必要的下载

--- vps/init.sh
+++ vps/init.sh
@@ -1,6 +1,5 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-curl -fsSL 'https://raw.githubusercontent.com/jiz4oh/shell/master/utils/log.sh' -o .tmp.sh && source .tmp.sh && rm .tmp.sh
 
 #=================================================
 #	System Required: CentOS/Debian/Ubuntu
@@ -19,54 +18,72 @@ curl -fsSL 'https://raw.githubusercontent.com/jiz4oh/shell/master/utils/log.sh' 
 timezone=Asia/Shanghai
 new_ssh_port=23232
 
+function info() { echo -e "\e[32m[info] $*\e[0m"; }
+function warn() { echo -e "\e[33m[warn] $*\e[0m"; }
+function error() { echo -e "\e[31m[error] $*\e[0m"; exit 1; }
+
+function check_depends() {
+  declare -a MISSING_PACKAGES
+
+  for depend in $*; do
+    command -v $depend &>/dev/null || MISSING_PACKAGES+=($depend)
+  done
+
+  if [ ! -z "${MISSING_PACKAGES}" ]; then
+    warn "The following is missing on the host and needs "
+    warn "to be installed and configured before running this script again"
+    error "missing: ${MISSING_PACKAGES[@]}"
+  fi
+}
+
 function setTimezone() {
-  LOG_INFO "set $1 as timezone"
+  info "set $1 as timezone"
   timedatectl set-timezone "$1"
 }
 
 function setSSH() {
-  LOG_DEBUG 'enable RSAAuthentication'
+  info 'enable RSAAuthentication'
   sed -in 's/.*RSAAuthentication.*/RSAAuthentication yes/g' /etc/ssh/sshd_config
 
-  LOG_DEBUG 'enable PubkeyAuthentication'
+  info 'enable PubkeyAuthentication'
   sed -in 's/.*PubkeyAuthentication.*/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
 
-  LOG_DEBUG 'set AuthorizedKeysFile'
+  info 'set AuthorizedKeysFile'
   sed -in 's/.*AuthorizedKeysFile.*/AuthorizedKeysFile .ssh\/authorized_keys/g' /etc/ssh/sshd_config
 
-  LOG_DEBUG "set $new_ssh_port as ssh port"
+  info "set $new_ssh_port as ssh port"
   sed -in "s/#Port 22/Port 22\nPort $new_ssh_port/g" /etc/ssh/sshd_config
   systemctl restart sshd
-  curl -fsSL 127.0.0.1:$new_ssh_port >/dev/null
+  curl -fsSL 127.0.0.1:$new_ssh_port &>/dev/null
 
   if [ 56 == $? ]; then
-    LOG_INFO 'new ssh port set success'
+    info 'new ssh port set success'
     sed -i '/Port 22/d' /etc/ssh/sshd_config
-    LOG_WARNING 'close the default ssh port(22) now'
-    LOG_WARNING "your new ssh port is $new_ssh_port, please remember it!!!"
+    warn 'close the default ssh port(22) now'
+    warn "your new ssh port is $new_ssh_port, please remember it!!!"
   else
-    LOG_ERROR 'new ssh port set fail'
-    LOG_ERROR 'may be firewall is running'
+    warn 'new ssh port set fail'
+    warn 'may be firewall is running'
   fi
 }
 
 function enableBBR() {
-  lsmod | grep bbr >/dev/null
-
-  if [ 0 == $? ]; then
-    LOG_INFO 'already enable bbr'
+  if [[ $(lsmod | grep bbr)]]; then
+    info 'already enable bbr'
   else
     echo -e "net.core.default_qdisc=fq\nnet.ipv4.tcp_congestion_control=bbr" >>/etc/sysctl.conf && sysctl -p
-    lsmod | grep bbr
 
-    if [ 0 == $? ]; then
-      LOG_INFO 'bbr enable success'
+    if [[ $(lsmod | grep bbr) ]]; then
+      info 'bbr enable success'
     else
-      LOG_ERROR 'bbr enable fail'
+      warn 'bbr enable fail'
     fi
   fi
 }
 
+#=================================== main ===================================
+check_depends curl timedatectl
 setTimezone $timezone
 setSSH $new_ssh_port
 enableBBR
+#=================================== main ===================================
